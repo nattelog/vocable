f10287beb644a1384f4dda79f451c7435d6b4370
Natanael Log <natte.log@gmail.com>
2016-12-10 17:34:22 +0100

    Use Saras exercises

--- build/client.js
+++ build/client.js
@@ -1,4 +1,5 @@
-{
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+module.exports={
   "1": {
     "q": "Vad menas med begreppet författning?",
     "a": "Författning är ett samlingsbegrepp för alla former av normgivning, som lagar, förordningar och föreskrifter. "
@@ -342,3 +343,297 @@
     "a": "Patientens namn och personnummer ska inte finnas med i det material som visas. Annars får de inte användas, om inte patienten samtycker till det. "
   }
 }
+
+},{}],2:[function(require,module,exports){
+"use strict";
+
+(function() {
+  var exercises = require('./exercises.json');
+
+  var exercisesPage = document.getElementById('exercises');
+  var exercisePage = document.getElementById('exercise');
+  var leftButton = document.getElementById('leftButton');
+  var rightButton = document.getElementById('rightButton');
+
+  var focusPage;
+  var focusExercise;
+
+  function reset() {
+    var keys = Object.keys(exercises);
+
+    keys.forEach(function(id) {
+      var exercise = exercises[id];
+
+      exercise.passed = false;
+      exercise.failed = false;
+      exercise.element.style.backgroundColor = '#337ab7';
+    });
+
+    showExercises();
+  }
+
+  function passExercise(id) {
+    var exercise = exercises[id];
+
+    if (!exercise) {
+      return console.error('exercise ' + id + ' does not exist');
+    }
+
+    exercise.failed = false;
+    exercise.passed = true;
+    exercise.element.style.backgroundColor = '#5cb85c';
+    showExercise();
+  }
+
+  function failExercise(id) {
+    var exercise = exercises[id];
+
+    if (!exercise) {
+      return console.error('exercise ' + id + ' does not exist');
+    }
+
+    exercise.failed = true;
+    exercise.passed = false;
+    exercise.element.style.backgroundColor = '#d9534f';
+    showExercise();
+  }
+
+  function getFailedExercises() {
+    var keys = Object.keys(exercises);
+    var result = [];
+
+    keys.forEach(function(id) {
+      var exercise = exercises[id];
+
+      if (!exercise.passed && exercise.failed) {
+        result.push(id);
+      }
+    });
+
+    if (!result.length) {
+      return undefined;
+    }
+
+    return result;
+  }
+
+  function getUnseenExercises() {
+    var keys = Object.keys(exercises);
+    var result = [];
+
+    keys.forEach(function(id) {
+      var exercise = exercises[id];
+
+      if (!exercise.passed && !exercise.failed) {
+        result.push(id);
+      }
+    });
+
+    if (!result.length) {
+      return undefined;
+    }
+
+    return result;
+  }
+
+  function getRandomExercise() {
+    var source = getUnseenExercises() || getFailedExercises();
+
+    if (!source) {
+      return false;
+    }
+
+    var randomIndex = Math.floor(Math.random() * source.length);
+
+    return source[randomIndex];
+  }
+
+  function updateButtons() {
+    var leftIcon = document.createElement('i');
+    var rightIcon = document.createElement('i');
+
+    leftButton.innerHTML = '';
+    rightButton.innerHTML = '';
+
+    if (focusPage === exercisePage) {
+      leftIcon.className = 'fa fa-thumbs-down';
+      rightIcon.className = 'fa fa-thumbs-up';
+      leftButton.className = 'btn btn-danger';
+      rightButton.className = 'btn btn-success';
+
+      leftButton.appendChild(leftIcon);
+      rightButton.appendChild(rightIcon);
+    }
+    else if (focusPage === exercisesPage) {
+      leftIcon.className = 'fa fa-repeat';
+      rightIcon.className = 'fa fa-random';
+      leftButton.className = 'btn btn-default';
+      rightButton.className = 'btn btn-default';
+
+      leftButton.appendChild(leftIcon);
+      rightButton.appendChild(rightIcon);
+    }
+  }
+
+  function updateStats() {
+    var passCount = document.getElementById('passCount');
+    var failCount = document.getElementById('failCount');
+    var unseenCount = document.getElementById('unseenCount');
+
+    var keys = Object.keys(exercises);
+    var total = keys.length;
+    var passed = 0;
+    var failed = 0;
+    var unseen = 0;
+
+    keys.forEach(function(id) {
+      var exercise = exercises[id];
+
+      if (exercise.passed) {
+        passed++;
+      }
+      else if (exercise.failed) {
+        failed++;
+      }
+      else {
+        unseen++;
+      }
+    });
+
+    var passedPerc = Math.round(100 * passed / total);
+    var failedPerc = Math.round(100 * failed / total);
+    var unseenPerc = Math.round(100 * unseen / total);
+    var rest = passedPerc + failedPerc + unseenPerc - 100;
+
+    if (rest !== 0) {
+      if (unseenPerc === 0) {
+        failedPerc = failedPerc - rest;
+      }
+      else {
+        unseenPerc = unseenPerc - rest;
+      }
+    }
+
+    passCount.style.width = passedPerc + '%';
+    failCount.style.width = failedPerc + '%';
+    unseenCount.style.width = unseenPerc + '%';
+
+    passCount.innerText = passed;
+    failCount.innerText = failed;
+    unseenCount.innerText = unseen;
+  }
+
+  function showPage(page) {
+    if (focusPage) {
+      focusPage.style.display = 'none';
+    }
+    page.style.display = 'block';
+    focusPage = page;
+    updateStats();
+    updateButtons();
+  }
+
+  function makeBlurry(element) {
+    element.style.color = 'transparent';
+    element.style.textShadow = '0 0 5px rgba(0, 0, 0, 0.5)';
+  }
+
+  function unBlur(element) {
+    element.style.color = 'black';
+    element.style.textShadow = 'none';
+  }
+
+  function showExercise(id) {
+    var _id = id || getRandomExercise();
+
+    if (!_id) {
+      return showExercises();
+    }
+
+    var exercise = exercises[_id];
+    var questionId = document.getElementById('questionId');
+    var question = document.getElementById('question');
+    var answer = document.getElementById('answer');
+
+    makeBlurry(answer);
+
+    questionId.innerText = 'Question ' + _id;
+    question.innerText = exercise.q;
+    answer.innerText = exercise.a;
+
+    focusExercise = _id;
+
+    showPage(exercisePage);
+  }
+
+  function showExercises() {
+    showPage(exercisesPage);
+  }
+
+  function leftButtonClick() {
+    if (focusPage === exercisesPage) {
+      reset();
+    }
+    else if (focusPage === exercisePage) {
+      failExercise(focusExercise);
+    }
+  }
+
+  function rightButtonClick() {
+    if (focusPage === exercisesPage) {
+      showExercise();
+    }
+    else if (focusPage === exercisePage) {
+      passExercise(focusExercise);
+    }
+  }
+
+  function populateTable() {
+    var table = document.getElementById('exercisesTable');
+    var keys = Object.keys(exercises);
+
+    keys.forEach(function(id) {
+      var exercise = exercises[id];
+      var item = document.createElement('span');
+
+      item.className = 'table-item badge';
+      item.id = 'exerciseItem_' + id;
+      item.innerHTML = id;
+      table.appendChild(item);
+
+      var button = document.getElementById('exerciseItem_' + id);
+      button.addEventListener('click', function(elem) {
+        showExercise(elem.target.innerText);
+      });
+
+      exercise.element = item;
+    });
+  }
+
+  function init() {
+    var exitButton = document.getElementById('exitButton');
+    var answer = document.getElementById('answer');
+
+    exercisePage.style.display = 'none';
+    exercisesPage.style.display = 'none';
+
+    populateTable();
+    showExercises();
+
+    answer.addEventListener('click', function() {
+      unBlur(this);
+    });
+
+    leftButton.addEventListener('click', leftButtonClick);
+
+    rightButton.addEventListener('click', rightButtonClick);
+
+    exitButton.addEventListener('click', function() {
+      showExercises();
+    });
+  }
+
+  init();
+})();
+
+},{"./exercises.json":1}]},{},[2]);
