11e93722fc2ce158b79ef5485eee1e84fbd298f0
Natanael Log <natte.log@gmail.com>
2017-03-22 17:55:38 +0100

    Add toggle QA

--- client.js
+++ client.js
@@ -956,169 +956,6 @@ module.exports = { '1': { q: 'praxis', a: 'vedertaget bruk' },
     };
   }
 
-  function updateButtons() {
-    var leftIcon = document.createElement('i');
-    var rightIcon = document.createElement('i');
-
-    leftButton.innerHTML = '';
-    rightButton.innerHTML = '';
-
-    if (focusPage === exercisePage) {
-      leftIcon.className = 'fa fa-thumbs-down';
-      rightIcon.className = 'fa fa-thumbs-up';
-      leftButton.className = 'btn btn-danger';
-      rightButton.className = 'btn btn-success';
-
-      leftButton.appendChild(leftIcon);
-      rightButton.appendChild(rightIcon);
-    }
-    else if (focusPage === exercisesPage) {
-      leftIcon.className = 'fa fa-repeat';
-      rightIcon.className = 'fa fa-random';
-
-      leftButton.className = 'btn btn-default';
-      rightButton.className = 'btn btn-default';
-
-      leftButton.appendChild(leftIcon);
-      rightButton.appendChild(rightIcon);
-    }
-  }
-
-  function updateStats() {
-    var passCount = document.getElementById('passCount');
-    var failCount = document.getElementById('failCount');
-    var unseenCount = document.getElementById('unseenCount');
-
-    var keys = Object.keys(exercises);
-    var total = keys.length;
-    var passed = 0;
-    var failed = 0;
-    var unseen = 0;
-
-    keys.forEach(function(id) {
-      var exercise = exercises[id];
-
-      if (exercise.passed) {
-        passed++;
-      }
-      else if (exercise.failed) {
-        failed++;
-      }
-      else {
-        unseen++;
-      }
-    });
-
-    var passedPerc = Math.round(100 * passed / total);
-    var failedPerc = Math.round(100 * failed / total);
-    var unseenPerc = Math.round(100 * unseen / total);
-    var rest = passedPerc + failedPerc + unseenPerc - 100;
-
-    if (rest !== 0) {
-      if (unseenPerc === 0) {
-        failedPerc = failedPerc - rest;
-      }
-      else {
-        unseenPerc = unseenPerc - rest;
-      }
-    }
-
-    passCount.style.width = passedPerc + '%';
-    failCount.style.width = failedPerc + '%';
-    unseenCount.style.width = unseenPerc + '%';
-
-    passCount.innerText = passed;
-    failCount.innerText = failed;
-    unseenCount.innerText = unseen;
-  }
-
-  function showPage(page) {
-    if (focusPage) {
-      focusPage.style.display = 'none';
-    }
-    page.style.display = 'block';
-    focusPage = page;
-    updateStats();
-    updateButtons();
-  }
-
-  function makeBlurry(element) {
-    element.style.color = 'transparent';
-    element.style.textShadow = '0 0 5px rgba(0, 0, 0, 0.5)';
-  }
-
-  function unBlur(element) {
-    element.style.color = 'black';
-    element.style.textShadow = 'none';
-  }
-
-  function showExercise(id) {
-    var _id = id || getRandomExercise();
-
-    if (!_id) {
-      return showExercises();
-    }
-
-    var exercise = exercises[_id];
-    var questionId = document.getElementById('questionId');
-    var question = document.getElementById('question');
-    var answer = document.getElementById('answer');
-
-    makeBlurry(answer);
-
-    questionId.innerText = 'Question ' + _id;
-    question.innerText = exercise.q;
-    answer.innerText = exercise.a;
-
-    focusExercise = _id;
-
-    showPage(exercisePage);
-  }
-
-  function showExercises() {
-    showPage(exercisesPage);
-  }
-
-  function leftButtonClick() {
-    if (focusPage === exercisesPage) {
-      reset();
-    }
-    else if (focusPage === exercisePage) {
-      failExercise(focusExercise);
-    }
-  }
-
-  function rightButtonClick() {
-    if (focusPage === exercisesPage) {
-      showExercise();
-    }
-    else if (focusPage === exercisePage) {
-      passExercise(focusExercise);
-    }
-  }
-
-  function populateTable() {
-    var table = document.getElementById('exercisesTable');
-    var keys = Object.keys(exercises);
-
-    keys.forEach(function(id) {
-      var exercise = exercises[id];
-      var item = document.createElement('span');
-
-      item.className = 'table-item badge';
-      item.id = 'exerciseItem_' + id;
-      item.innerHTML = id;
-      table.appendChild(item);
-
-      var button = document.getElementById('exerciseItem_' + id);
-      button.addEventListener('click', function(elem) {
-        showExercise(elem.target.innerText);
-      });
-
-      exercise.element = item;
-    });
-  }
-
   function Button(buttonClass, iconClass, onClick) {
     var button = document.createElement('button'); 
     var icon = document.createElement('icon');
@@ -1307,9 +1144,16 @@ module.exports = { '1': { q: 'praxis', a: 'vedertaget bruk' },
       // setup question and answer
       var question = document.createElement('h3');
       var answer = document.createElement('p');
+      var toggleQA = Math.random() >= 0.5;
 
-      question.innerHTML = exercise.question;
-      answer.innerHTML = exercise.answer;
+      if (toggleQA) {
+        question.innerHTML = exercise.question;
+        answer.innerHTML = exercise.answer;
+      }
+      else {
+        question.innerHTML = exercise.answer;
+        answer.innerHTML = exercise.question;
+      }
 
       // make answer blurry at first
       answer.style.color = 'transparent';
